f22940a63b05215891afa171b9d46bb8e37e1ae5
eleahburman <eleahjburman@gmail.com>
2023-07-24 19:43:10 -0400

    Add class for accounts

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('<h1>Weekly Finances</h1>')
+  return HttpResponse(request, 'accounts/index.html', {'accounts': accounts})
 
 def bio(request):
   return render(request, 'bio.html')
+
+class Account:
+  self.bank = bank
+  self.checking = checking
+  self.saving = saving 
+  
+accounts = [
+  Account('TD Bank', 2100.00, 1000.00),
+  Account('Chase', 1500.00, 1000.00)
+]
